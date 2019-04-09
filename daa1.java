4aae0f2c3ae2e352ed0d194dfda8664187a307c2
Apoorva <Apoorva>
2019-04-09 21:59:14 +0530

    Uploaded java code

---
+++ daa1.java
@@ -0,0 +1,481 @@
+import java.util.ArrayList;
+import java.util.Collections;
+ class City {
+    int x;
+    int y;
+    
+    // Constructs a randomly placed city
+    public City(){
+        this.x = (int)(Math.random()*200);
+        this.y = (int)(Math.random()*200);
+    }
+    
+    // Constructs a city at chosen x, y location
+    public City(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    // Gets city's x coordinate
+    public int getX(){
+        return this.x;
+    }
+    
+    // Gets city's y coordinate
+    public int getY(){
+        return this.y;
+    }
+    
+    // Gets the distance to given city
+    public double distanceTo(City city){
+        int xDistance = Math.abs(getX() - city.getX());
+        int yDistance = Math.abs(getY() - city.getY());
+        double distance = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance) );
+        
+        return distance;
+    }
+    
+    @Override
+    public String toString(){
+        return getX()+", "+getY();
+    }
+}
+
+
+
+class TourManager {
+
+    // Holds our cities
+    private static ArrayList destinationCities = new ArrayList<City>();
+
+    // Adds a destination city
+    public static void addCity(City city) {
+        destinationCities.add(city);
+    }
+    
+    // Get a city
+    public static City getCity(int index){
+        return (City)destinationCities.get(index);
+    }
+    
+    // Get the number of destination cities
+    public static int numberOfCities(){
+        return destinationCities.size();
+    }
+}
+
+
+
+
+
+
+ class Tour{
+
+    // Holds our tour of cities
+    private ArrayList tour = new ArrayList<City>();
+    // Cache
+    private double fitness = 0;
+    private int distance = 0;
+    
+    // Constructs a blank tour
+    public Tour(){
+        for (int i = 0; i < TourManager.numberOfCities(); i++) {
+            tour.add(null);
+        }
+    }
+    
+    public Tour(ArrayList tour){
+        this.tour = tour;
+    }
+
+    // Creates a random individual
+    public void generateIndividual() {
+        // Loop through all our destination cities and add them to our tour
+        for (int cityIndex = 0; cityIndex < TourManager.numberOfCities(); cityIndex++) {
+          setCity(cityIndex, TourManager.getCity(cityIndex));
+        }
+        // Randomly reorder the tour
+        Collections.shuffle(tour);
+    }
+
+    // Gets a city from the tour
+    public City getCity(int tourPosition) {
+        return (City)tour.get(tourPosition);
+    }
+
+    // Sets a city in a certain position within a tour
+    public void setCity(int tourPosition, City city) {
+        tour.set(tourPosition, city);
+        // If the tours been altered we need to reset the fitness and distance
+        fitness = 0;
+        distance = 0;
+    }
+    
+    // Gets the tours fitness
+    public double getFitness() {
+        if (fitness == 0) {
+            fitness = 1/(double)getDistance();
+        }
+        return fitness;
+    }
+    
+    // Gets the total distance of the tour
+    public int getDistance(){
+        if (distance == 0) {
+            int tourDistance = 0;
+            // Loop through our tour's cities
+            for (int cityIndex=0; cityIndex < tourSize(); cityIndex++) {
+                // Get city we're travelling from
+                City fromCity = getCity(cityIndex);
+                // City we're travelling to
+                City destinationCity;
+                // Check we're not on our tour's last city, if we are set our 
+                // tour's final destination city to our starting city
+                if(cityIndex+1 < tourSize()){
+                    destinationCity = getCity(cityIndex+1);
+                }
+                else{
+                    destinationCity = getCity(0);
+                }
+                // Get the distance between the two cities
+                tourDistance += fromCity.distanceTo(destinationCity);
+            }
+            distance = tourDistance;
+        }
+        return distance;
+    }
+
+    // Get number of cities on our tour
+    public int tourSize() {
+        return tour.size();
+    }
+    
+    // Check if the tour contains a city
+    public boolean containsCity(City city){
+        return tour.contains(city);
+    }
+    
+    @Override
+    public String toString() {
+        String geneString = "|";
+        for (int i = 0; i < tourSize(); i++) {
+            geneString += getCity(i)+"|";
+        }
+        return geneString;
+    }
+}
+
+
+
+class Population {
+
+    // Holds population of tours
+    Tour[] tours;
+
+    // Construct a population
+    public Population(int populationSize, boolean initialise) {
+        tours = new Tour[populationSize];
+        // If we need to initialise a population of tours do so
+        if (initialise) {
+            // Loop and create individuals
+            for (int i = 0; i < populationSize(); i++) {
+                Tour newTour = new Tour();
+                newTour.generateIndividual();
+                saveTour(i, newTour);
+            }
+        }
+    }
+    
+    // Saves a tour
+    public void saveTour(int index, Tour tour) {
+        tours[index] = tour;
+    }
+    
+    // Gets a tour from population
+    public Tour getTour(int index) {
+        return tours[index];
+    }
+
+    // Gets the best tour in the population
+    public Tour getFittest() {
+        Tour fittest = tours[0];
+        // Loop through individuals to find fittest
+        for (int i = 1; i < populationSize(); i++) {
+            if (fittest.getFitness() <= getTour(i).getFitness()) {
+                fittest = getTour(i);
+            }
+        }
+        return fittest;
+    }
+
+    // Gets population size
+    public int populationSize() {
+        return tours.length;
+    }
+}
+
+
+
+ class GA {
+
+    /* GA parameters */
+    private static final double mutationRate = 0.015;
+    private static final int tournamentSize = 5;
+    private static final boolean elitism = true;
+
+    // Evolves a population over one generation
+    public static Population evolvePopulation(Population pop) {
+        Population newPopulation = new Population(pop.populationSize(), false);
+
+        // Keep our best individual if elitism is enabled
+        int elitismOffset = 0;
+        if (elitism) {
+            newPopulation.saveTour(0, pop.getFittest());
+            elitismOffset = 1;
+        }
+
+        // Crossover population
+        // Loop over the new population's size and create individuals from
+        // Current population
+        for (int i = elitismOffset; i < newPopulation.populationSize(); i++) {
+            // Select parents
+           // Tour parent1 = tournamentSelection(pop);
+            //Tour parent2 = tournamentSelection(pop);
+
+
+
+
+               Tour[] temp_a = roullet( newPopulation);
+            if( temp_a == null )
+        {
+            System.out.println("no a");
+            //r//eturn temp;
+        }
+           // Tour parent1 = tournamentSelection(pop);
+            //Tour parent2 = tournamentSelection(pop);
+          Tour  parent1 = temp_a[0];
+            Tour parent2 = temp_a[1];
+
+
+
+
+
+
+
+
+            // Crossover parents
+            Tour child = crossover(parent1, parent2);
+            // Add child to new population
+            newPopulation.saveTour(i, child);
+        }
+
+        // Mutate the new population a bit to add some new genetic material
+        for (int i = elitismOffset; i < newPopulation.populationSize(); i++) {
+            mutate(newPopulation.getTour(i));
+        }
+
+        return newPopulation;
+    }
+
+    // Applies crossover to a set of parents and creates offspring
+    public static Tour crossover(Tour parent1, Tour parent2) {
+        // Create new child tour
+        Tour child = new Tour();
+
+        // Get start and end sub tour positions for parent1's tour
+        int startPos = (int) (Math.random() * parent1.tourSize());
+        int endPos = (int) (Math.random() * parent1.tourSize());
+
+        // Loop and add the sub tour from parent1 to our child
+        for (int i = 0; i < child.tourSize(); i++) {
+            // If our start position is less than the end position
+            if (startPos < endPos && i > startPos && i < endPos) {
+                child.setCity(i, parent1.getCity(i));
+            } // If our start position is larger
+            else if (startPos > endPos) {
+                if (!(i < startPos && i > endPos)) {
+                    child.setCity(i, parent1.getCity(i));
+                }
+            }
+        }
+
+        // Loop through parent2's city tour
+        for (int i = 0; i < parent2.tourSize(); i++) {
+            // If child doesn't have the city add it
+            if (!child.containsCity(parent2.getCity(i))) {
+                // Loop to find a spare position in the child's tour
+                for (int ii = 0; ii < child.tourSize(); ii++) {
+                    // Spare position found, add city
+                    if (child.getCity(ii) == null) {
+                        child.setCity(ii, parent2.getCity(i));
+                        break;
+                    }
+                }
+            }
+        }
+        return child;
+    }
+
+    // Mutate a tour using swap mutation
+    private static void mutate(Tour tour) {
+        // Loop through tour cities
+        for(int tourPos1=0; tourPos1 < tour.tourSize(); tourPos1++){
+            // Apply mutation rate
+            if(Math.random() < mutationRate){
+                // Get a second random position in the tour
+                int tourPos2 = (int) (tour.tourSize() * Math.random());
+
+                // Get the cities at target position in tour
+                City city1 = tour.getCity(tourPos1);
+                City city2 = tour.getCity(tourPos2);
+
+                // Swap them around
+                tour.setCity(tourPos2, city1);
+                tour.setCity(tourPos1, city2);
+            }
+        }
+    }
+
+    // Selects candidate tour for crossover
+    private static Tour tournamentSelection(Population pop) {
+        // Create a tournament population
+        Population tournament = new Population(tournamentSize, false);
+        // For each place in the tournament get a random candidate tour and
+        // add it
+        for (int i = 0; i < tournamentSize; i++) {
+            int randomId = (int) (Math.random() * pop.populationSize());
+            tournament.saveTour(i, pop.getTour(randomId));
+        }
+        // Get the fittest tour
+        Tour fittest = tournament.getFittest();
+        return fittest;
+    }
+
+
+
+
+
+     private static Tour[] roullet(Population pop) {
+        
+        Tour[] temp = new Tour[2];
+        double[] arr = new double[pop.populationSize()];
+        int i;
+        double total_fitness = 0;
+        
+        int n = pop.populationSize();
+        for(i=0;i<n;i++)
+        {
+
+            if(pop.getTour(i) == null )
+        {
+            System.out.println("no popppp"+pop.tours.length);
+            return temp;
+        }
+            double a = pop.getTour(i).getFitness();
+            total_fitness += a;
+        }
+        for(i=0;i<n;i++)
+        {
+            double a =pop.getTour(i).getFitness();
+            arr[i] = a/total_fitness;
+            
+        }
+        double rn = Math.random();
+        double diff = 100000000;
+        int index1=0,index2=0;
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]<rn && (rn - arr[i])<diff)
+            {
+                   index1 = i;
+                   diff = rn - arr[i];
+            }
+            
+        }
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]>rn && ( arr[i] - rn  )<diff)
+            {
+                   index2 = i;
+                   diff = arr[i]-rn;
+            }
+            
+        }
+        
+        temp[0] = pop.getTour(index1);
+        temp[1] = pop.getTour(index2);
+        
+        return temp;
+        
+    }
+
+
+
+
+
+}
+
+
+ class TSP_GA {
+
+    public static void main(String[] args) {
+
+        // Create and add our cities
+        City city = new City(60, 200);
+        TourManager.addCity(city);
+        City city2 = new City(180, 200);
+        TourManager.addCity(city2);
+        City city3 = new City(80, 180);
+        TourManager.addCity(city3);
+        City city4 = new City(140, 180);
+        TourManager.addCity(city4);
+        City city5 = new City(20, 160);
+        TourManager.addCity(city5);
+        City city6 = new City(100, 160);
+        TourManager.addCity(city6);
+        City city7 = new City(200, 160);
+        TourManager.addCity(city7);
+        City city8 = new City(140, 140);
+        TourManager.addCity(city8);
+        City city9 = new City(40, 120);
+        TourManager.addCity(city9);
+        City city10 = new City(100, 120);
+        TourManager.addCity(city10);
+        City city11 = new City(180, 100);
+        TourManager.addCity(city11);
+        City city12 = new City(60, 80);
+        TourManager.addCity(city12);
+        City city13 = new City(120, 80);
+        TourManager.addCity(city13);
+        City city14 = new City(180, 60);
+        TourManager.addCity(city14);
+        City city15 = new City(20, 40);
+        TourManager.addCity(city15);
+        City city16 = new City(100, 40);
+        TourManager.addCity(city16);
+        City city17 = new City(200, 40);
+        TourManager.addCity(city17);
+        City city18 = new City(20, 20);
+        TourManager.addCity(city18);
+        City city19 = new City(60, 20);
+        TourManager.addCity(city19);
+        City city20 = new City(160, 20);
+        TourManager.addCity(city20);
+
+        // Initialize population
+        Population pop = new Population(50, true);
+        System.out.println("Initial distance: " + pop.getFittest().getDistance());
+
+        // Evolve population for 100 generations
+        pop = GA.evolvePopulation(pop);
+        for (int i = 0; i < 100; i++) {
+            pop = GA.evolvePopulation(pop);
+        }
+
+        // Print final results
+        System.out.println("Finished");
+        System.out.println("Final distance: " + pop.getFittest().getDistance());
+        System.out.println("Solution:");
+        System.out.println(pop.getFittest());
+    }
+}
